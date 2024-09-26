11184a2de09653e13b2aeb1086ed4eed8cfd2dfc
XD-cods <vkuzir7@gmail.com>
2024-09-26 12:11:46 +0300

    init project

---
+++ src/main/java/org/example/modsentest/ModsenTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.modsentest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModsenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModsenTestApplication.class, args);
+	}
+
+}
